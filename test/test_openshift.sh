fa7f8076fcf84cb4c46b481907275c70e8421a43
Jan Scholz <jan.scholz@gmail.com>
2019-05-16 19:34:14 +0200

    ephemeral mysql

--- test/test_openshift.sh
+++ test/test_openshift.sh
@@ -15,9 +15,17 @@ GREP "version 18.09.2"
 RUNS docker-compose version
 GREP "version 1.23.2"
 
-RUNS docker volume create mysql_data
-RUNS docker volume list
-GREP mysql_data
-RUNS docker volume remove mysql_data
-RUNS docker volume list
-NGREP mysql_data
+#RUNS docker volume create mysql_data
+#RUNS docker volume list
+#GREP mysql_data
+
+#RUNS docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mypassword -v mysql_data:/var/lib/mysql mysql:5.6 --character-set-server=utf8mb4 --collation-server=utf8mb4_general_ci
+RUNS docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mypassword mysql:5.6 --character-set-server=utf8mb4 --collation-server=utf8mb4_general_ci
+MYSQLCONTAINER=$(cat $OSHT_STDOUT)
+echo MYSQLCONTAINER: $MYSQLCONTAINER
+
+RUNS docker kill "$MYSQLCONTAINER"
+
+#RUNS docker volume remove mysql_data
+#RUNS docker volume list
+#NGREP mysql_data
